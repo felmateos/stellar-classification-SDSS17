c7fb8e73a75c16a491d88275e633a740f345e75a
felmateos <felmateos@usp.br>
2024-06-29 21:14:25 -0300

    Improved learning_curve visualization

--- modeling/validation/validation_funcs.py
+++ modeling/validation/validation_funcs.py
@@ -16,7 +16,7 @@ def plot_learning_curve(estimator, X, y, cv, scoring='accuracy'):
     
     train_sizes, train_scores, test_scores = learning_curve(
         estimator, X, y, cv=cv, scoring=scoring, n_jobs=-1,
-        train_sizes=np.linspace(0.1, 1.0, 10)
+        train_sizes=np.exp(np.linspace(np.log(0.0005), np.log(1.0), 10))
     )
     
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -29,8 +29,8 @@ def plot_learning_curve(estimator, X, y, cv, scoring='accuracy'):
     plt.ylim(0.0, 1.1)
     plt.grid()
     
-    plt.plot(train_sizes, train_scores_mean, 'o-', color='c', label='Pontuação de Treinamento')
-    plt.plot(train_sizes, test_scores_mean, 'o-', color='m', label='Pontuação de Validação') 
+    plt.plot(train_sizes, train_scores_mean, '.-', color='c', label='Pontuação de Treinamento')
+    plt.plot(train_sizes, test_scores_mean, '.-', color='m', label='Pontuação de Validação') 
     
     plt.legend(loc="best")
     plt.show()
